bfc0250a9daef82958205579c3eaaf830de175a0
Koomawat <52982332+Koomawat@users.noreply.github.com>
2021-01-30 22:42:46 -0500

    Update MazeGen.py
    updated hard coded issues

--- MazeGen.py
+++ MazeGen.py
@@ -5,10 +5,9 @@ from matplotlib import colors
 
 def mazeGen():
 
+    # Taking size of the maze from the user
     maze_length = int(input("Enter the length of a square maze: "))
-    # Hard coded dim x dim matrix to represent the Maze
     maze = np.zeros((maze_length, maze_length))
-    
 
     # Variable p represents the probability of a cell in the Maze being occupied
     p = 0.3
@@ -24,16 +23,16 @@ def mazeGen():
     # An occupied cell is represented as a 1
     # A fire cell is represented as a 2
     maze[0,0] = 0
-    maze[9,9] = 0
+    maze[maze_length-1,maze_length-1] = 0
 
     # Here we find a random free cell in the Maze for the initial fire position
     # tuple1, tuple2 represent the 
     tuple1 = 0
     tuple2 = 0
-    # We don't want the tuples to be 0,0 or 9,9 as those cells are the start and end of the Maze
-    while (tuple1 == 0 and tuple2 == 0) or (tuple1 == 9 or tuple2 == 9):
-        tuple1 = random.randint(1,9)
-        tuple2 = random.randint(1,9)
+    # We don't want the tuples to be 0,0 or length-1,length-1 as those cells are the start and end of the Maze
+    while (tuple1 == 0 and tuple2 == 0) or (tuple1 == (maze_length-1) and tuple2 == (maze_length-1)):
+        tuple1 = random.randint(1,maze_length-1)
+        tuple2 = random.randint(1,maze_length-1)
 
         # Checking if the tuples generated are an occupied cell
         # If so we reset the tuples to 0 so we keep looping until we find a random free cell
