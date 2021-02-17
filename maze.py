0b0c8866a4c90d6183b31a9824879228984d77dc
Jane <sp1716@rutgers.edu>
2021-02-17 02:17:48 -0500

    organizing

--- maze.py
+++ maze.py
@@ -10,6 +10,50 @@ from matplotlib import colors
 
 def main():
 
+    plotting()
+
+    # main_maze, mlen = mazeGen.mazeGen()
+    # # Using matplotlib to visualize the Maze in a grid view
+    # # plt.imshow(main_maze)
+    # # plt.show()
+    # main_maze, fire_initial = mazeGen.initialFire(main_maze, len(main_maze))
+
+    # start = (0,0)
+    # goal = (mlen-1, mlen-1)
+
+    # # dfs checks if one point to another is reachable or not
+    # fire_reachable = dfs(main_maze, start, fire_initial, mlen)
+    # print(f'Is there a path from agent to fire {fire_initial} using DFS?: {fire_reachable}')
+    # reachable = dfs(main_maze, start, goal, mlen)
+    # print(f'Is there a path from {start} to {goal} using DFS?: {reachable}')
+
+    #bfs returns the optimal path from the start to the goal
+    #optimalpath = bfs(main_maze, start, goal, mlen)
+    #print(f'Was a shortest path found from {start} to {goal}?: {optimalpath}')
+
+    #colored_maze = colorPath(optimalpath, main_maze)
+    #plt.imshow(colored_maze)
+    #plt.show()
+
+    # # A* returning the optimal path from S to G
+    # aStarPath = aStar(main_maze, start, goal)
+    # print(f'Is there a path from {start} to {goal} using A*?: {aStarPath}')
+    # color_maze = colorPath(aStarPath, main_maze, 0, 0)
+    # plt.imshow(color_maze)
+    # plt.show()
+
+    #fired = future_fire(color_maze)
+    #plt.imshow(fired)
+    #plt.show()
+
+    #answer2 = stratTwoAStar(main_maze, aStarPath, goal)
+    #print (answer2)
+
+    # answer3 = stratThreeAStar(main_maze, aStarPath, goal)
+    # print (answer3)
+
+
+def plotting():
     flammability = [x * 0.1 for x in range(1, 10)]
     average = []
 
@@ -63,49 +107,6 @@ def main():
     plt.ylabel('Average success')
     plt.title('Strategy 2 of A* Algorithm')
     plt.show()
-
-
-
-    # main_maze, mlen = mazeGen.mazeGen()
-    # # Using matplotlib to visualize the Maze in a grid view
-    # # plt.imshow(main_maze)
-    # # plt.show()
-    # main_maze, fire_initial = mazeGen.initialFire(main_maze, len(main_maze))
-
-    # start = (0,0)
-    # goal = (mlen-1, mlen-1)
-
-    # # dfs checks if one point to another is reachable or not
-    # fire_reachable = dfs(main_maze, start, fire_initial, mlen)
-    # print(f'Is there a path from agent to fire {fire_initial} using DFS?: {fire_reachable}')
-    # reachable = dfs(main_maze, start, goal, mlen)
-    # print(f'Is there a path from {start} to {goal} using DFS?: {reachable}')
-
-    #bfs returns the optimal path from the start to the goal
-    #optimalpath = bfs(main_maze, start, goal, mlen)
-    #print(f'Was a shortest path found from {start} to {goal}?: {optimalpath}')
-
-    #colored_maze = colorPath(optimalpath, main_maze)
-    #plt.imshow(colored_maze)
-    #plt.show()
-
-    # # A* returning the optimal path from S to G
-    # aStarPath = aStar(main_maze, start, goal)
-    # print(f'Is there a path from {start} to {goal} using A*?: {aStarPath}')
-    # color_maze = colorPath(aStarPath, main_maze, 0, 0)
-    # plt.imshow(color_maze)
-    # plt.show()
-
-    #fired = future_fire(color_maze)
-    #plt.imshow(fired)
-    #plt.show()
-
-    #answer2 = stratTwoAStar(main_maze, aStarPath, goal)
-    #print (answer2)
-
-    # answer3 = stratThreeAStar(main_maze, aStarPath, goal)
-    # print (answer3)
-
 
 if __name__ == "__main__":
     main()
